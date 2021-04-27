b389bab065a4a99a9f3fbd19a424a720a53b0c25
taehwakkwon <taehwak@hanyang.ac.kr>
2021-04-27 19:32:49 +0000

    code_refactoring

---
+++ 권태확/model.py
@@ -0,0 +1,142 @@
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+from torchvision import transforms
+from torchvision.models import vgg16
+
+class FCN8s(nn.Module):
+    def __init__(self, num_classes):
+        super(FCN8s,self).__init__()
+        self.pretrained_model = vgg16(pretrained = True)
+        features, classifiers = list(self.pretrained_model.features.children()), list(self.pretrained_model.classifier.children())
+
+        self.features_map1 = nn.Sequential(*features[0:17])
+        self.features_map2 = nn.Sequential(*features[17:24])
+        self.features_map3 = nn.Sequential(*features[24:31])
+        
+        # Score pool3
+        self.score_pool3_fr = nn.Conv2d(256, num_classes, 1)
+        
+        # Score pool4        
+        self.score_pool4_fr = nn.Conv2d(512, num_classes, 1)        
+        
+        # fc6 ~ fc7
+        self.conv = nn.Sequential(nn.Conv2d(512, 4096, kernel_size = 1),
+                                  nn.ReLU(inplace=True),
+                                  nn.Dropout(),
+                                  nn.Conv2d(4096, 4096, kernel_size = 1),
+                                  nn.ReLU(inplace=True),
+                                  nn.Dropout()
+                                  )
+        
+        # Score
+        self.score_fr = nn.Conv2d(4096, num_classes, kernel_size = 1)
+        
+        # UpScore2 using deconv
+        self.upscore2 = nn.ConvTranspose2d(num_classes,
+                                           num_classes,
+                                           kernel_size=4,
+                                           stride=2,
+                                           padding=1)
+        
+        # UpScore2_pool4 using deconv
+        self.upscore2_pool4 = nn.ConvTranspose2d(num_classes, 
+                                                 num_classes, 
+                                                 kernel_size=4,
+                                                 stride=2,
+                                                 padding=1)
+        
+        # UpScore8 using deconv
+        self.upscore8 = nn.ConvTranspose2d(num_classes, 
+                                           num_classes,
+                                           kernel_size=16,
+                                           stride=8,
+                                           padding=4)
+    
+    def forward(self, x):
+        pool3 = h = self.features_map1(x)
+        pool4 = h = self.features_map2(h)
+        h = self.features_map3(h)
+        
+        h = self.conv(h)
+        h = self.score_fr(h)
+       
+        score_pool3c = self.score_pool3_fr(pool3)    
+        score_pool4c = self.score_pool4_fr(pool4)
+        
+        # Up Score I
+        upscore2 = self.upscore2(h)
+        
+        # Sum I
+        h = upscore2 + score_pool4c
+        
+        # Up Score II
+        upscore2_pool4c = self.upscore2_pool4(h)
+        
+        # Sum II
+        h = upscore2_pool4c + score_pool3c
+        
+        # Up Score III
+        upscore8 = self.upscore8(h)
+        
+        return upscore8
+
+
+class DoubleConv(nn.Module):
+    def __init__(self, in_channels, out_channels):
+        super(DoubleConv, self).__init__()
+        self.conv = nn.Sequential(
+            nn.Conv2d(in_channels, out_channels, 3, 1, 1 ,bias=False),
+            nn.BatchNorm2d(out_channels),
+            nn.ReLU(inplace=True),
+            nn.Conv2d(out_channels, out_channels, 3, 1, 1, bias=False),
+            nn.BatchNorm2d(out_channels),
+            nn.ReLU(inplace=True),
+        )
+
+    def forward(self, x):
+        return self.conv(x)
+
+
+class UNet(nn.Module):
+    
+    def __init__(self, in_channels=3, out_channels=1, features=[64, 128, 256, 512]):
+        super(UNet, self).__init__()
+        self.downs = nn.ModuleList()
+        self.ups = nn.ModuleList()
+        self.pool = nn.MaxPool2d(2, 2)
+
+        for feature in features:
+            self.downs.append(DoubleConv(in_channels, feature))
+            in_channels = feature
+
+        self.bottleneck = DoubleConv(features[-1], features[-1]*2)
+
+        for feature in features[::-1]:
+            self.ups.append(
+                nn.ConvTranspose2d(2*feature, feature, 2, 2)
+                )
+            self.ups.append(DoubleConv(2*feature, feature))
+        
+        self.final_conv = nn.Conv2d(features[0], out_channels, 1)
+
+    def forward(self, x):
+        skip_connections = []
+
+        for down in self.downs:
+            x = down(x)
+            skip_connections.append(x)
+            x = self.pool(x)
+
+        x = self.bottleneck(x)
+        skip_connections = skip_connections[::-1]
+        
+        for idx in range(0, len(self.ups), 2):
+            x = self.ups[idx](x)
+            skip_connection = skip_connections[idx//2]
+            if x.shape != skip_connection.shape:
+                x = transforms.functional.center_crop(x, output_size=skip_connection.shape[-2:])
+            concat = torch.cat((skip_connection, x), dim=1)
+            x = self.ups[idx+1](concat)
+        return self.final_conv(x)
+
